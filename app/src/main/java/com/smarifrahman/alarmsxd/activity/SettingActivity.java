d4ed93b4bda06b17a5852b4928ddbc68915b6335
arif-rahman-cse <smrahmanarif905@gmail.com>
2020-02-08 15:05:29 +0600

    setting done

--- app/src/main/java/com/smarifrahman/alarmsxd/activity/SettingActivity.java
+++ app/src/main/java/com/smarifrahman/alarmsxd/activity/SettingActivity.java
@@ -1,16 +1,31 @@
 package com.smarifrahman.alarmsxd.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.smarifrahman.alarmsxd.R;
+import com.smarifrahman.alarmsxd.databinding.ActivitySettingBinding;
 
 public class SettingActivity extends AppCompatActivity {
+
+    ActivitySettingBinding mSettingBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        mSettingBinding = DataBindingUtil.setContentView(this, R.layout.activity_setting);
+
+        mSettingBinding.settingBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingActivity.this, HomeActivity.class));
+                finish();
+            }
+        });
     }
 }
